7a17b0bbfb02c4416e4701006a88e96a610b0daa
Ching Wu <j2u0d3y1@gmail.com>
2021-06-19 21:55:36 +0800

    second commit

--- password.py
+++ password.py
@@ -1,12 +1,12 @@
 password = "a123456"
 i = 3 #剩餘機會
-while True:
+while i > 0:
+	i = i - 1
 	密碼 = input('請輸入密碼')
 	if 密碼 == password:
 		print('登入成功')
 		break #逃出迴圈
-	else:
-		i = i - 1 
-		print('密碼錯誤! 還有' , i, '次機會')
-		if i == 0:
-			break
+	else: 
+		print('密碼錯誤!')
+		if i > 0:
+			print(' 還有' , i, '次機會')
